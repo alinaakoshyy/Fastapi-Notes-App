e7676efc69a0f9396103d8075941e3937eb605f7
alinaakoshyy <koshyalina@gmail.com>
2025-07-08 17:45:52 +0530

    Added update_note endpoint and fixed get_note by ID

--- fastapi2.py
+++ fastapi2.py
@@ -4,6 +4,8 @@ from fastapi import FastAPI
 from typing import List, Optional
 from pydantic import BaseModel
 from datetime import datetime
+from fastapi import status
+from fastapi import HTTPException
 
 app = FastAPI()
 
@@ -26,7 +28,7 @@ notes: List[Fullnote] = []
 next_note_id = 1
 
 
-@app.post("/add_note")
+@app.post("/add_note", status_code=status.HTTP_201_CREATED)
 def add_note(note: NoteIn):  # NoteIn is a Pydantic model for input validation
     global next_note_id
     new_note = Fullnote(
@@ -42,14 +44,28 @@ def add_note(note: NoteIn):  # NoteIn is a Pydantic model for input validation
 
 
 @app.get("/get_notes")
-def get_notes(search: Optional[str] = None):
-    if search:    # If a search query is provided, filter the notes 
-        search_lower = search.lower()  # Convert search term to lowercase for case-insensitive comparison     
+def get_notes(search: Optional[str] = None, note_id: Optional[int] = None):
+    if search:    # If a search query is provided, filter the notes
+        # Convert search term to lowercase for case-insensitive comparison
+        search_lower = search.lower()
         filtered_notes = [
-            note for note in notes 
-            if search_lower in note.title.lower() or search_lower in note.content.lower()] # case-insensitive search
+            note for note in notes
+            # case-insensitive search
+            if search_lower in note.title.lower() or search_lower in note.content.lower()]
         return {"notes": filtered_notes}
     return {"notes": notes}    # If no search query, return all notes
+
+from fastapi import HTTPException
+
+@app.get("/get_note/{note_id}")
+def get_note(note_id: int):
+    for note in notes:
+        if note.note_id == note_id:
+            return note
+    raise HTTPException(
+        status_code=404,
+        detail=f"Note with ID {note_id} not found"
+    )
 
 
 @app.put("/update_note/{note_id}")
@@ -68,5 +84,8 @@ def delete_note(note_id: int):
         if note.note_id == note_id:
             notes.remove(note)
             return {"message": f"Note with ID {note_id} deleted successfully"}
-    # If the note ID is not found, return an error message
-    return {"error": "Note ID out of range"}
+
+    raise HTTPException(
+        status_code=404,
+        detail=f"Note not found"
+    )
